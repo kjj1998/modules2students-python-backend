e9808cc1a8efcc78593a1df4f097432f7c96cd70
Koh Jun Jie <koh.jun.jie@outlook.com>
2023-12-22 12:58:27 +0800

    removed obsolete dependency code

--- app/routers/modules.py
+++ app/routers/modules.py
@@ -6,23 +6,9 @@ from neo4j import Driver
 from dependencies import get_db_driver  # pylint: disable=import-error
 from . import crud
 
-
-# URI = "neo4j://localhost:7687"
-# AUTH = ("neo4j", "12345678")
-
 router = APIRouter(
     prefix="/modules", tags=["modules"], responses={404: {"description": "Not found"}}
 )
-
-# async def get_db_driver():
-#     """Dependency to Neo4j db driver"""
-#     driver = GraphDatabase.driver(URI, auth=AUTH)
-#     driver.verify_connectivity()
-#     try:
-#         yield driver
-#     finally:
-#         driver.close()
-
 
 @router.get("/")
 async def read_modules(skip: int, limit: int, driver: Driver = Depends(get_db_driver)):
