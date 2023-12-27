39234a5e3f3dd3a33b77719c263d0c636582d992
Koh Jun Jie <koh.jun.jie@outlook.com>
2023-12-27 16:17:39 +0800

    refactored all module endpoints into its own directory

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 Main module of the module2student Python backend
 """
 from fastapi import FastAPI
-from routers import modules # pylint: disable=import-error
+from routers.modules import modules # pylint: disable=import-error
 
 app = FastAPI()
 
