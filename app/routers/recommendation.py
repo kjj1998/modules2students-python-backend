19db602b83d9a47d93aa7526a701d5ee61a10eb8
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-01-21 11:59:59 +0800

    changed imports for reccommendation router

--- app/routers/recommendation.py
+++ app/routers/recommendation.py
@@ -5,22 +5,26 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from neo4j import Driver
-from ..dependencies import get_db_driver  # pylint: disable=import-error
-from .models import StudentBase, RecommendationModel
-from .recommendation_services import get_recommendations
+
+from ..dependencies import get_db_driver
+from ..models.rec import Recommendation
+from ..services.rec import get_recommendations
 
 router = APIRouter(
-    prefix="/recommendations", tags=["recommendations"], responses={404: {"description": "Not found"}}
+    prefix="/recommendations",
+    tags=["recommendations"],
+    responses={404: {"description": "Not found"}},
 )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
-@router.get("/{student_id}", response_model=RecommendationModel)
+
+@router.get("/{student_id}", response_model=Recommendation)
 async def retrieve_recommendations(
     token: Annotated[str, Depends(oauth2_scheme)],
     student_id: str | None = None,
     driver: Driver = Depends(get_db_driver),
-) -> RecommendationModel:
+) -> Recommendation:
     """API endpoint to get a particular student's recommendations"""
 
     if str is None:
